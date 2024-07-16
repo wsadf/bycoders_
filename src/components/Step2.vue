03c7aaa77c9e248b931baf701fd0f2d259e7e8c4
Wesley Soares <wesley@tecnoig.com.br>
2024-07-16 16:29:22 -0300

    feat(components) adicionando validacao de telefone

--- src/components/Step2.vue
+++ src/components/Step2.vue
@@ -14,13 +14,23 @@
           @blur="validateCpf"
         />
         <span v-if="cpfError" class="error">{{ cpfError }}</span>
-        
+
         <label for="nascimento">Data de nascimento:</label>
-        <input id="nascimento" v-model="props.formData.nascimento" @input="maskDateInput" />
+        <input
+          id="nascimento"
+          v-model="props.formData.nascimento"
+          @input="maskDateInput"
+        />
         <span v-if="dateError" class="error">{{ dateError }}</span>
 
         <label for="telefone">Número de telefone:</label>
-        <input id="telefone" v-model="props.formData.telefone" type="text" />
+        <input
+          id="telefone"
+          v-model="props.formData.telefone"
+          type="text"
+          @input="maskPhoneNumber"
+        />
+        <span v-if="telefoneError" class="error">{{ telefoneError }}</span>
       </div>
 
       <div v-if="props.formData?.cadastroType === 'PJ'">
@@ -61,6 +71,7 @@ const props = defineProps({
 
 let cpfError = ref("");
 let dateError = ref("");
+let telefoneError = ref("");
 
 const formatCpf = (cpf) => {
   cpf = cpf.replace(/\D/g, "");
@@ -83,32 +94,32 @@ const validateCpf = () => {
 
 const maskDateInput = () => {
   let maskedValue = props.formData.nascimento
-    .replace(/\D/g, '')
-    .replace(/(\d{2})(\d)/, '$1/$2')
-    .replace(/(\d{2})(\d)/, '$1/$2')
-    .replace(/(\d{4})\d*/, '$1');
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1/$2")
+    .replace(/(\d{2})(\d)/, "$1/$2")
+    .replace(/(\d{4})\d*/, "$1");
   props.formData.nascimento = maskedValue;
 
   // Limpar dateError se uma data válida for inserida
   if (maskedValue.length === 10) {
     validateDate();
   } else {
-    dateError.value = '';
+    dateError.value = "";
   }
 };
 
 const validateDate = () => {
   const inputValue = props.formData.nascimento.trim();
 
-  if (inputValue === '') {
-    dateError.value = 'Data de nascimento é obrigatória';
+  if (inputValue === "") {
+    dateError.value = "Data de nascimento é obrigatória";
   } else {
-    const d1 = Date.parse(inputValue.replace(/([0-9]+)\/([0-9]+)/, '$2/$1'));
+    const d1 = Date.parse(inputValue.replace(/([0-9]+)\/([0-9]+)/, "$2/$1"));
 
     if (isNaN(d1)) {
-      dateError.value = 'Data de nascimento inválida';
+      dateError.value = "Data de nascimento inválida";
     } else {
-      dateError.value = '';
+      dateError.value = "";
     }
   }
 };
@@ -144,17 +155,43 @@ const isValidCpf = (cpf) => {
   return true;
 };
 
+const maskPhoneNumber = () => {
+  let phone_number = props.formData.telefone;
+  phone_number = phone_number.replace(/\D/g, '');
+
+  // Aplicar a máscara (00) 0000-0000 ou (00) 00000-0000
+  if (phone_number.length <= 10) {
+    props.formData.telefone = phone_number.replace(/^(\d{2})(\d)/g, '($1) $2').replace(/(\d{4})(\d)/, '$1-$2');
+  } else {
+    props.formData.telefone = phone_number.replace(/^(\d{2})(\d)/g, '($1) $2').replace(/(\d{5})(\d)/, '$1-$2');
+  }
+
+  // Validar o telefone
+  validatePhoneNumber(phone_number);
+};
+
+const validatePhoneNumber = (phone_number) => {
+  if (phone_number.length < 10) {
+    telefoneError.value = "Número de telefone inválido";
+  } else {
+    telefoneError.value = "";
+  }
+};
+
 const isFormValid = computed(() => {
-  return !cpfError.value && !dateError.value;
+  return !cpfError.value && !dateError.value && !telefoneError.value && props.formData.telefone.trim() !== "";
 });
 
 const handleNextStep = () => {
   validateCpf();
   validateDate();
+  validatePhoneNumber(props.formData.telefone.replace(/\D/g, ''));
 
-  if (!cpfError.value && !dateError.value) {
+  if (!cpfError.value && !dateError.value && !telefoneError.value && props.formData.telefone.trim() !== "") {
     props.updateFormData(props.formData);
     props.nextStep();
+  } else if (props.formData.telefone.trim() === "") {
+    telefoneError.value = "Favor inserir um telefone";
   }
 };
 </script>
