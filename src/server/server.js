a92fb0c9c742b69ec9f1fdf49709deac7cb250ac
Wesley Soares <wesley@tecnoig.com.br>
2024-07-22 16:47:50 -0300

    feat(components) ajustes server

--- src/server/server.js
+++ src/server/server.js
@@ -6,15 +6,19 @@ const app = express();
 app.use(cors());
 
 app.get('/registration', (req, res) => {
-    res.redirect('/registration');
+    try {
+        res.status(200).json({ message: 'Bem-vindo à rota de registro.' });
+    } catch (e) {
+        res.status(500).json({ message: 'Erro interno do servidor.', error: e.message });
+    }
 });
 
 app.post('/registration', (req, res) => {
     try {
-        return res.status(201).json({ message: 'Usuário Cadastrado.'});
+        return res.status(201).json({ message: 'Usuário Cadastrado.' });
     } catch (e) {
         return res.status(500).json({ message: 'Erro interno do servidor.', error: e.message });
     }
 });
 
-app.listen(9000,() => console.log('server js'))
+app.listen(9000, () => console.log('Server is running on port 9000'));
